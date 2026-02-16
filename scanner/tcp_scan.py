a10d3b3cb1fd7622a95712f7a6f30ea5fa2b1967
Glen Boghe <r1002150@ucll.be>
2026-02-16 12:39:02 +0100

    multithreading port_scan (improves performance)

--- scanner/tcp_scan.py
+++ scanner/tcp_scan.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 def validate_target(target_ip):
@@ -45,34 +46,48 @@ def grab_banner(sock, port):
 
     except Exception:
         return "Error retrieving banner"
+    
+def scan_single_port(target_ip, port):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(2)
+            result = sock.connect_ex((target_ip, port))
+
+            if result == 0:
+                banner = grab_banner(sock, port)
+                return port, banner
+    except Exception:
+        pass
+
+    return None
 
 
 def scan_ports(target_ip, start_port, end_port):
     print(f"\nScanning {target_ip} ({start_port}-{end_port})...")
     start_time = time.time()
-    open_ports = 0
+    open_ports = []
+ 
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        futures = [
+            executor.submit(scan_single_port, target_ip, port)
+            for port in range(start_port, end_port + 1)
+        ]
 
-    for port in range(start_port, end_port + 1):
-        try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                sock.settimeout(2)
-                result = sock.connect_ex((target_ip, port))
-
-                if result == 0:
-                    banner = grab_banner(sock, port)
-                    print(f"OPEN PORT: {port} - Banner: {banner}")
-                    open_ports += 1
-
-        except Exception:
-            pass
+        for future in as_completed(futures):
+            result = future.result()
+            if result:
+                port, banner = result
+                open_ports.append(port)
+                print(f"OPEN PORT: {port} - Banner: {banner}")
 
     end_time = time.time()
 
     print(f"\nScan completed in {end_time - start_time:.2f} seconds")
-    if open_ports == 0:
+
+    if not open_ports:
         print("No open ports found.")
     else:
-        print(f"{open_ports} open port(s) found.")
+        print(f"{len(open_ports)} open port(s) found.")
 
 
 def main():
